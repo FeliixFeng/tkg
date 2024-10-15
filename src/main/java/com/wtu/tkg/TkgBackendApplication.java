d6ae7e94932abed2f111351f81a310282e5400a6
zachary_liu <uu_cloth@126.com>
2024-10-15 13:25:35 +0800

    项目初始框架

---
+++ src/main/java/com/wtu/tkg/TkgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wtu.tkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TkgBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wtu/tkg/TkgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtu.tkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TkgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
